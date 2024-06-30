2b249d9767e597efaa2a7d576587664eb9857d6a
asila99 <alisa.abdulova1309@gmail.com>
2024-06-30 17:52:17 +0500

    start project

---
+++ src/Project/commands/DeleteCommand.java
@@ -0,0 +1,27 @@
+package Project.commands;
+
+import Project.exceptions.CommandException;
+
+import java.io.File;
+
+public class DeleteCommand implements Command {
+    @Override
+    public void execute(String args) throws CommandException {
+        String[] parts = args.split(" ", 2);
+        if (parts.length != 2) {
+            throw new CommandException("Error, enter command one more time.");
+        }
+
+        String fileName = parts[0];
+        String directoryPath = parts[1];
+        File file = new File(directoryPath, fileName);
+
+        if (!file.exists()) {
+            throw new CommandException("Error, file is not found.");
+        } else if (file.delete()) {
+            System.out.println("Done, file " + fileName + " is deleted.");
+        } else {
+            throw new CommandException("Error, could not delete file.");
+        }
+    }
+}
